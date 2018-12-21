d87d9aa6da042df440495aff9b83f781d69c4a96
rtak <rtak@e-sites.nl>
2018-12-21 11:59:17 +0100

    Basic layout done and made a start with the prisma server

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,7 +1,38 @@
 import Link from 'next/link';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import styled from 'styled-components';
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+};
+
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+};
+
+Router.onRouteChangeError = () => {
+  NProgress.done();
+};
+
+const StyledHeader = styled.nav`
+  display: flex;
+  justify-content: center;
+  border-bottom: 2px solid ${props => props.theme.black};
+  margin: 0 auto;
+
+  a {
+    font-family: ${props => props.theme.fontHeading};
+    padding: 1rem;
+
+    &:hover {
+      color: ${props => props.theme.primary};
+    }
+  }
+`;
 
 const Header = () => (
-  <div>
+  <StyledHeader>
     <Link href="/">
       <a>Home</a>
     </Link>
@@ -14,7 +45,7 @@ const Header = () => (
     <Link href="/aspects">
       <a>Aspects</a>
     </Link>
-  </div>
+  </StyledHeader>
 );
 
 export default Header;
--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,15 +1,74 @@
 import React, { Component } from 'react';
+import styled, { ThemeProvider, createGlobalStyle } from 'styled-components';
 import Header from './Header';
 import Meta from './Meta';
+
+const theme = {
+  primary: '#E0335E',
+  secondary: '#FF8100',
+  black: '#393939',
+  white: '#fff',
+  maxWidth: '1000px',
+  fontRegular: `'Alegreya-Regular', 'Arial', 'sans-serif'`,
+  fontHeading: `'FjallaOne-Regular', 'Arial', 'sans-serif'`,
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)'
+};
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'FjallaOne-Regular';
+    src: url('/static/FjallaOne-Regular.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'Alegreya-Regular';
+    src: url('/static/Alegreya-Regular.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+  }
+  html {
+    box-sizing: border-box;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    line-height: 1.2;
+    font-family: 'Alegreya-Regular';
+    color: ${theme.black};
+    background-color: ${theme.white};
+  }
+  h1, h2, h3, h4, h5, h6 {
+    font-family: 'FjallaOne-Regular';
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+`;
+
+const InnerPage = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 class Page extends Component {
   render() {
     return (
-      <div>
-        <Meta />
-        <Header />
-        {this.props.children}
-      </div>
+      <ThemeProvider theme={theme}>
+        <React.Fragment>
+          <GlobalStyle />
+          <Meta />
+          <Header />
+          <InnerPage>{this.props.children}</InnerPage>
+        </React.Fragment>
+      </ThemeProvider>
     );
   }
 }
